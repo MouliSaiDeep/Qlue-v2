d375e4316c05ebb556e97473c63cae5db96380be
Mouli Sai Deep <moulisaideep.get@gmail.com>
2026-03-09 23:51:26 +0530

    Defined:Backend Skeleton

---
+++ qlue_backend/src/test/java/com/qlue/qlue_backend/QlueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlue.qlue_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
